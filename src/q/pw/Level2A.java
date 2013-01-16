1d9f4b857022dccb6d6f113e6e14ad886aaab100
Administrator <Administrator@172.18.3.36>
2013-01-16 14:49:42 +0800

    Q

--- src/q/pw/Level2A.java
+++ src/q/pw/Level2A.java
@@ -17,11 +17,13 @@ public class Level2A extends ListActivity {
 		//
 		d.add(new Pw("qq", "12325140、123816"));
 		d.add(new Pw("tenpay", "12325140、123816"));
-		
 		d.add(new Pw("taobao", "q123816"));
+		//
 		d.add(new Pw("elance", "haiq87"));
 		d.add(new Pw("facebook", "haiq87"));
 		d.add(new Pw("twitter", "haiq87"));
+		d.add(new Pw("evernote", "haiq87"));
+		d.add(new Pw("google", "haiq1987"));
 		//
 		setListAdapter(new PwAdapter(this, d));
 	}
